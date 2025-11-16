cc80ffe50bd26f884ccba0b21251c16093692e11
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-11-16 15:14:41 +0900

    Delete tt.py

--- tt.py
+++
@@ -1,4 +0,0 @@
-import redis
-r = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
-print(r.hget("synonym_rules", "만족도"))
-print(r.hget("dictionary_rules:SYNONYM", "만족도"))
